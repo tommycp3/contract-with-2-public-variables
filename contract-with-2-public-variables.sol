cad015f1413f962f8fd29229c78f9155befe2f6e
Thomas Miller <thomasmiller@Thomass-MBP.gateway>
2017-12-23 12:08:43 +1000

    simple contract with 2 public variables

---
+++ contract-with-2-public-variables.sol
@@ -0,0 +1,9 @@
+contract User {
+    string public userName;
+    string public userName2;
+    
+    function User(string _name, string _name2) {
+        userName = _name;
+        userName2 = _name2;
+    }
+}
